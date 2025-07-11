2497ea05c39d8e516f9b571d6dcb246bed53cfcd
Matheus Hack <matheushacksv@gmail.com>
2025-07-11 10:40:52 -0300

    Projeto original

---
+++ portfolio/dados.py
@@ -0,0 +1,44 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Python'
+    },
+    'habilidade2': {
+        'nome': 'Django'
+    },
+    'habilidade3': {
+        'nome': 'Git e Github'
+    },
+    'habilidade4': {
+        'nome': 'HTML'
+    },
+}
+
+projetos = {
+    'projeto1': {
+        'titulo': 'Contratos Automáticos com IA',
+        'descricao': 'Criação de contratos jurídicos automáticos com dados dinâmicos do cliente em questão',
+        'tecnologia': 'Python',
+        'github': '',
+    },
+
+    'projeto2': {
+        'titulo': 'Dashboard de dados do Meta Ads',
+        'descricao': 'Projeto de visualização de dados com dados atualizados em tempo real de campanhas do MetaAds e vendas/leads reais com requisição em bancos de dados',
+        'tecnologia': 'Python, Streamlit, MongoDB, Facebook SDK',
+        'github': 'https://dashboardkit.henriquegoularte.com.br/',
+    },
+
+    'projeto3': {
+        'titulo': 'Oráculo',
+        'descricao': 'Um chatbot que sabe tudo sobre a empresa usando RAG',
+        'tecnologia': 'Python, Streamlit, LangChain',
+        'github': '',
+    },
+
+    'projeto4': {
+        'titulo': 'Portfolio',
+        'descricao': 'Este portfolio foi totalmente feito em django, python e HTML 😉',
+        'tecnologia': 'Python, Django, HTML',
+        'github': '',
+    }
+}
---
+++ portfolio/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .dados import habilidades, projetos
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', {'habilidades': habilidades})
+
+def lista_projetos(request):
+    return render(request, 'projetos.html', {'projetos': projetos})
+
+def detalhes_projeto(request, id_projeto):
+    projeto = projetos.get(id_projeto)
+    return render(request, 'detalhes_projeto.html', {'projeto': projeto})
